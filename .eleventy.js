ec6cfd69bae3b224820094ed0fa976f8854e5c9e
Trevor Morris <code@trovster.com>
2023-09-29 10:30:50 +0100

    fix: add code example

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,27 @@
 require('dotenv').config()
 
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+
 const dateFormat = require('./src/_filters/date')
 const absoluteUrl = require('./src/_filters/absoluteUrl')
+const stringify = require('./src/_filters/stringify')
 const { number, miles, percent, max } = require('./src/_filters/numbers')
 
 module.exports = config => {
+  config.addPlugin(syntaxHighlight)
+
   config.addFilter('dateFormat', dateFormat)
   config.addFilter('absoluteUrl', absoluteUrl)
+  config.addFilter('stringify', stringify)
   config.addFilter('formattedNumber', number)
   config.addFilter('miles', miles)
   config.addFilter('percent', percent)
   config.addFilter('max', max)
 
   config.addPassthroughCopy('CNAME')
+  config.addPassthroughCopy({
+    'node_modules/prismjs/themes/prism-okaidia.min.css': 'css/prism-theme.css'
+  })
 
   return {
     dir: {
--- README.md
+++ README.md
@@ -1,9 +1,36 @@
 # Fitness Web Component
 
-Simple Web Components for fitness card and rings.
+Simple Web Components for fitness cards and rings.
 
 The components are built styled using [Tailwind](https://tailwindcss.com) and
 the icons are from [Heroicons](https://heroicons.com). The design is based on
 Apple Fitness.
 
 This website is built using [Eleventy](https://www.11ty.dev).
+
+## The Code
+
+The components loop through the `fitness.json` dataset.
+
+Each component expects a JSON payload shape that looks like the following.
+
+```json
+{
+    "date": "2023-09-25T00:00:00+00:00",
+    "step_count": 18935,
+    "distance": 16254,
+    "flights_climbed": 8,
+    "move": {
+        "total": 500,
+        "goal": 750
+    },
+    "exercise": {
+        "total": 30,
+        "goal": 60
+    },
+    "stand": {
+        "total": 8,
+        "goal": 10
+    }
+}
+```
