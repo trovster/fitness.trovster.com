a1050db70567694cb5c7f277f0950f751bf7f5c6
Trevor Morris <code@trovster.com>
2023-09-30 17:25:25 +0100

    chore: change from health to fitness for health title

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 
+const { EleventyRenderPlugin } = require('@11ty/eleventy')
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
 
 const dateFormat = require('./src/_filters/date')
@@ -9,6 +10,12 @@ const { number, miles, percent, max } = require('./src/_filters/numbers')
 
 module.exports = config => {
   config.addPlugin(syntaxHighlight)
+  config.addPlugin(EleventyRenderPlugin)
+
+  config.addShortcode('svg', async function (filename, options = {}) {
+    const { renderFile } = config.javascriptFunctions
+    return await renderFile(`./src/icons/${filename}.svg`, options, 'njk')
+  })
 
   config.addFilter('dateFormat', dateFormat)
   config.addFilter('absoluteUrl', absoluteUrl)
--- src/_filters/numbers.js
+++ src/_filters/numbers.js
@@ -8,9 +8,11 @@ module.exports = {
     return number * 0.000621371192
   },
   percent: (data, fractions = 0) => {
+    if (! data || ! data.total || ! data.goal) return 0;
     return (data.total / data.goal * 100).toFixed(fractions)
   },
   max: (number, max = 100) => {
+    if (! number) return 0;
     return number >= max ? max : number
   }
 }
